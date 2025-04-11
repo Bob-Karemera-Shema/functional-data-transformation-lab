f47d560daeaa556ebd9b0d659a866d44f57f1dd1
Bob Karemera Shema <bobshema14@gmail.com>
2025-04-11 09:52:06 +0200

    added a function isPalindrome() to check if a string is a palindrome

--- strings.js
+++ strings.js
@@ -11,3 +11,19 @@ function reverse(str) {
     }
     return result;
 }
+
+function isPalindrome(str) {
+    // change string to lowercase and remove whitespaces to avoid edge cases
+    str = str.toLowerCase().replaceAll(" ", "");
+
+    let left = 0;
+    let right = str.length - 1;
+
+    while(left <= right) {
+        if(str[left] !== str[right]) return false;
+        left += 1;
+        right -= 1;
+    }
+
+    return true;
+}
