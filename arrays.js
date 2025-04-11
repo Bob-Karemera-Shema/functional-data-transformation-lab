436a7459a40e82a83b34a19cbbca75717c6b6121
Bob Karemera Shema <bobshema14@gmail.com>
2025-04-11 11:17:17 +0200

    added the double() function that doubles every number in an array

---
+++ arrays.js
@@ -0,0 +1,7 @@
+function double(arr) {
+    // use map() to iterate through each array element and multiply it by 2
+    // return the new array
+    return arr.map(number => number * 2);
+}
+
+console.log(double([1,2,3,4,5]));
