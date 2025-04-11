28ba7b0caa5f2b459c82be593560c8d24b15ecbb
Bob Karemera Shema <bobshema14@gmail.com>
2025-04-11 11:50:50 +0200

    Added the sum() function that calculates the sum of all numbers in an array

--- arrays.js
+++ arrays.js
@@ -1,12 +1,28 @@
 function double(arr) {
-    // use map() to iterate through each array number and multiply it by 2
-    // return the new array
-    return arr.map(number => number * 2);
+    // if the argument is not an array or an empty array, return it
+    if((!Array.isArray(arr)) || arr.length === 0) return arr;
+
+    // use map() to iterate through each array number and multiply it by 2, and return the new array
+    // check for non-number types to avoid edge cases
+    return arr.map(number => typeof(number) === 'number' ? number * 2 : number);
 }
 
 function filterEven(arr) {
+    // if the argument is not an array or an empty array, return it
+    if((!Array.isArray(arr)) || arr.length === 0) return arr;
+
     // use filter() to iterate through the array and return an array with even numbers
-    return arr.filter(number => number % 2 === 0);
+    // check for non-number types to avoid edge cases
+    return arr.filter(number => typeof(number) === 'number' && number % 2 === 0);
 }
 
-console.log(filterEven([1, 2, 3, 4, 5, 22, 13, 15]));
+function sum(arr) {
+    // if the argument is not an array or an empty array, return it
+    if((!Array.isArray(arr)) || arr.length === 0) return arr;
+
+    // use reduce() to iterate through the array numbers and return a sum
+    // check for non-number types to avoid edge cases
+    return arr.reduce((acc, number) => typeof(number) === 'number' ? acc += number : acc+= 0, 0);
+}
+
+console.log(filterEven([1, 'avd', 3]));
