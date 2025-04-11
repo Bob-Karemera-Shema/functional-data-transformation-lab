a0d29002eac887aedcf92ead2987542f449b2004
Bob Karemera Shema <bobshema14@gmail.com>
2025-04-11 12:26:49 +0200

    Added the average() function that calculates the average of all numbers in an array

--- arrays.js
+++ arrays.js
@@ -1,28 +1,46 @@
+function isNotValid(arr) {
+    // This functions checks the following:
+    // 1. if the argument is not an array
+    // 2. if the argument is an empty array
+    // 3. if the argument is an array that contains elements which are not numbers
+    return (!Array.isArray(arr) || arr.length === 0 || arr.some(el => typeof el !== 'number'));
+}
+
 function double(arr) {
-    // if the argument is not an array or an empty array, return it
-    if((!Array.isArray(arr)) || arr.length === 0) return arr;
+    // if argument is not valid, return it
+    if(isNotValid(arr)) return arr;
 
     // use map() to iterate through each array number and multiply it by 2, and return the new array
     // check for non-number types to avoid edge cases
-    return arr.map(number => typeof(number) === 'number' ? number * 2 : number);
+    return arr.map(number => number * 2);
 }
 
 function filterEven(arr) {
-    // if the argument is not an array or an empty array, return it
-    if((!Array.isArray(arr)) || arr.length === 0) return arr;
+    // if argument is not valid, return it
+    if(isNotValid(arr)) return arr;
 
     // use filter() to iterate through the array and return an array with even numbers
     // check for non-number types to avoid edge cases
-    return arr.filter(number => typeof(number) === 'number' && number % 2 === 0);
+    return arr.filter(number => number % 2 === 0);
 }
 
 function sum(arr) {
-    // if the argument is not an array or an empty array, return it
-    if((!Array.isArray(arr)) || arr.length === 0) return arr;
+    // if argument is not valid, return it
+    if(isNotValid(arr)) return arr;
 
     // use reduce() to iterate through the array numbers and return a sum
     // check for non-number types to avoid edge cases
-    return arr.reduce((acc, number) => typeof(number) === 'number' ? acc += number : acc+= 0, 0);
+    return arr.reduce((acc, number) => acc += number, 0);
 }
 
-console.log(filterEven([1, 'avd', 3]));
+function average(arr) {
+    // if argument is not valid, return it
+    if(isNotValid(arr)) return arr;
+
+    // get the sum of all elements
+    const arraySum = sum(arr);
+
+    return arraySum/arr.length;
+}
+
+console.log(average([1, 2, 3]));
