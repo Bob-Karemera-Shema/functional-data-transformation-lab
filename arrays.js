d4f691f3ae327b007c7bb73599c9c1b5ea737980
Bob Karemera Shema <bobshema14@gmail.com>
2025-04-11 11:22:35 +0200

    added the filterEven() function that filters out even numbers from an array

--- arrays.js
+++ arrays.js
@@ -1,7 +1,12 @@
 function double(arr) {
-    // use map() to iterate through each array element and multiply it by 2
+    // use map() to iterate through each array number and multiply it by 2
     // return the new array
     return arr.map(number => number * 2);
 }
 
-console.log(double([1,2,3,4,5]));
+function filterEven(arr) {
+    // use filter() to iterate through the array and return an array with even numbers
+    return arr.filter(number => number % 2 === 0);
+}
+
+console.log(filterEven([1, 2, 3, 4, 5, 22, 13, 15]));
