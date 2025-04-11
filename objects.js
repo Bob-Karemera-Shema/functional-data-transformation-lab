1b1449ff4ad74a717f5ed6b616715cb0349f348f
Bob Karemera Shema <bobshema14@gmail.com>
2025-04-11 14:50:53 +0200

    Added the isAdult() function that checks if a person is 18 or older

--- objects.js
+++ objects.js
@@ -1,9 +1,18 @@
 function fullName(person) {
     // check if the input is not null and check if the properties exist
-    // if not, return the input
+    // if not, return error message
     if(!person || !person.firstName || ! person.lastName) return 'invalid input or missing object properties';
 
+    // return the full name string
     return `${person.firstName} ${person.lastName}`;
 }
 
-console.log(fullName({firstName: 'Bob', lastName: 'Karemera'}));
+function isAdult(person) {
+    // check if the input is not null and check if the age property exist
+    // if not, return error message
+    if(!person || !person.age) return 'invalid input or missing object properties';
+
+    return person.age >= 18;
+}
+
+console.log(isAdult());
